3da7f8a89473d124f87295089eb7c9eafeefa6af
elinapark0818@gmail.com <dbswjd64>
2020-11-20 11:10:48 +0900

    Study in Company.201120

--- com/yunjung/study/Company.py
+++ com/yunjung/study/Company.py
@@ -32,11 +32,47 @@ print("="*10)
 
 print("{0: >10}".format(500)) # 오른쪽으로 정렬
 print("{0:_<10}".format(500)) # 왼쪽으로 정렬 빈자리 "_"로 채우기
+
 print('='*20)
+
 print("{0:,}".format(10000000000000)) # 3자리마다 콤마 찍기
 print("{0:+,}".format(10000000000000)) # 3자리마다 콤마 찍고 +- 부호 붙이기
 print("{0:+,}".format(-10000000000000))
+
 print('='*20)
+
 print("{0:^<+30,}".format(10000000000000)) # 3자리마다 콤마 찍고, 빈자리 "^"로 채우고 30자리 확보하고 왼쪽정렬하고 +- 부호 붙이기
 print("{0:^<+30,}".format(-10000000000000))
+
 print('='*20)
+
+print("{0:f}".format(5/3)) # 소수점 출력
+print("{0:.2f}".format(5/3)) # .2를 f앞에 입력하면 소수점 셋째에서 반올림
+
+print('='*20)
+
+# score_file = open("score.txt", "w", encoding="utf8")
+# print("수학 : 0", file=score_file)
+# print("영어 : 50", file=score_file)
+# score_file.close()
+
+# score_file = open("score.txt", "a", encoding="utf8")
+# score_file.write("과학 : 80")
+# score_file.write("\n코딩 : 100")
+# score_file.close()
+#
+score_file = open("score.txt", "r", encoding="utf8")
+print(score_file.readline()) # 줄별로 읽기, 한줄 읽고 커서는 다음줄로 이동
+print(score_file.readline())
+print(score_file.readline())
+print(score_file.readline())
+score_file.close()
+
+print('='*20)
+
+score_file = open("score.txt", "r", encoding="utf8")
+print(score_file.readline(), end="") # 줄바꿈 없이
+print(score_file.readline(), end="")
+print(score_file.readline(), end="")
+print(score_file.readline(), end="")
+score_file.close()
