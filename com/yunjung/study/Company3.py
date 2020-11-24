d1e5ea31b6aa29ed2a4ec4f3415b226c45a66452
elinapark0818@gmail.com <elinapark0818@gmail.com>
2020-11-24 14:50:23 +0900

    Study in Company.201124

--- com/yunjung/study/Company3.py
+++ com/yunjung/study/Company3.py
@@ -35,15 +35,47 @@
 # wraith2.clocking = True
 # if wraith2.clocking == True:
 #     print("{0} 는 현재 클로킹 상태입니다.".format(wraith2.name))
+#
+# class AttackUnit:
+#     def __init__(self, name, hp, damage):
+#         self.name = name
+#         self.hp = hp
+#         self.damage = damage
+#
+#     def attack(self, location):
+#         print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}"\
+#               .format(self.name, location, self.damage))
+#
+#     def damaged(self, damage):
+#         print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+#         self.hp -= damage
+#         print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+#         if self.hp <= 0:
+#             print("{0} : 파괴되었습니다.".format(self.name))
+#
+# firebat1 = AttackUnit("파이어뱃", 50, 16)
+# firebat1.attack("5시")
+#
+# firebat1.damaged(25)
+# firebat1.damaged(25)
 
-class AttackUnit:
-    def __init__(self, name, hp, damage):
+print('=' * 10)
+
+class Unit:  # 일반 유닛
+    def __init__(self, name, hp):
         self.name = name
         self.hp = hp
+
+marine = Unit("마린", 40)
+tank = Unit("탱크", 150)
+
+class AttackUnit(Unit):  # 공격 유닛
+    def __init__(self, name, hp, damage):
+        Unit.__init__(self, name, hp)
         self.damage = damage
 
     def attack(self, location):
-        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}"\
+        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}] " \
               .format(self.name, location, self.damage))
 
     def damaged(self, damage):
@@ -53,30 +85,28 @@ class AttackUnit:
         if self.hp <= 0:
             print("{0} : 파괴되었습니다.".format(self.name))
 
-firebat1 = AttackUnit("파이어뱃", 50, 16)
-firebat1.attack("5시")
+monster = AttackUnit("괴물", 100, 20)
+monster.attack("1시")
+monster.damaged(25)
+monster.damaged(25)
+monster.damaged(25)
+monster.damaged(25)
 
-firebat1.damaged(25)
-firebat1.damaged(25)
+print('=' * 10)
 
-print('='*10)
 
-class Unit:
-    def __init__(self, name, hp):
-        Unit.__init__(self,name,hp)
-        self.damage = damage
-    def attack(self, location):
-        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}" \
-              .format(self.name, location, self.damage))
-    def damaged(self, damaged):
-
-class Flyable:
+class Flyable:  # 날 수 있는 기능을 가진 클래스
     def __init__(self, flying_speed):
         self.flying_speed = flying_speed
+
     def fly(self, name, location):
-        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}"\
+        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]" \
               .format(name, location, self.flying_speed))
-class FlyableAttackUnit(AttackUnit, Flyable):
+
+class FlyableAttackUnit(AttackUnit, Flyable):  # 공중 공격 유닛 클래스
     def __init__(self, name, hp, damage, flying_speed):
         AttackUnit.__init__(self, name, hp, damage)
         Flyable.__init__(self, flying_speed)
+
+valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+valkyrie.fly(valkyrie.name, "3시")
