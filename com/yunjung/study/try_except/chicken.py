2dd4a175dfc72aa0235cf77e6ba49cb1b147d446
ElinaPark <elinapark0818@gmail.com>
2020-11-29 00:08:14 +0900

    feat(chicken.py) 대기번호 프로그램 개발중

--- com/yunjung/study/try_except/chicken.py
+++ com/yunjung/study/try_except/chicken.py
@@ -1,17 +1,17 @@
-
 class SoldOutError(Exception):
     pass
 
+
 chicken = 10
-waiting = 1  #대기번호는 1번부터 시작한다
-while(True):
+waiting = 1  # 대기번호는 1번부터 시작한다
+while (True):
     try:
         print("[남은 치킨 : {0}]".format(chicken))
         order = int(int(input("치킨 몇 마리 주문하시겠습니까?")))
-        if order > chicken:  #남은 치킨보다 주문량이 많을 때
+        if order > chicken:  # 남은 치킨보다 주문량이 많을 때
             print("재료가 부족합니다.")
         elif order <= 0:
-            raise  ValueError
+            raise ValueError
         else:
             print("[대기번호 {0}] {1} 마리 주문이 완료되었습니다."\
                   .format(waiting, order))
