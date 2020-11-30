9c539828a718bdaac37d995dff31e447e6a30e92
elinapark0818@gmail.com <elinapark0818@gmail.com>
2020-11-30 17:40:24 +0900

    다중상속 연습

--- com/yunjung/study/Company/201130.py
+++ com/yunjung/study/Company/201130.py
@@ -1,12 +1,18 @@
 
 class Unit:
-    def __init__(self, name, hp):
+    def __init__(self, name, hp, speed):
         self.name = name
         self.hp = hp
+        self.speed = speed
+
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]"\
+              .format(self.name, location, self.speed))
 
 class AttackUnit(Unit):
-    def __init__(self, name, hp, damage):
-        Unit.__init__(self, name, hp)
+    def __init__(self, name, hp, speed, damage):
+        Unit.__init__(self, name, hp, speed)
         self.damage = damage
 
     def attack(self, location):
@@ -34,8 +40,19 @@ class Flyable:
 
 class FlyableAttackUnit(AttackUnit, Flyable):
     def __init__(self, name, hp, damage, flying_speed):
-        AttackUnit.__init__(self, name, hp, damage)
+        AttackUnit.__init__(self, name, hp, 0, damage)  #지상 speed 0
         Flyable.__init__(self, flying_speed)
 
-valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
-valkyrie.fly(valkyrie.name, "3시")
+# valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+# valkyrie.fly(valkyrie.name, "3시")
+
+# 연산자 오버라이딩  메소드 오버라이딩
+
+# 벌쳐 : 지상 유닛, 기동성이 좋음
+vulture = AttackUnit("벌쳐", 80, 10, 20)
+
+# 배틀크루저 :  공중유닛, 체력이 굉장히 좋음, 공격력도 좋음
+battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
+
+vulture.move("11시")
+battlecruiser.fly(battlecruiser.name, "9시")
