d19cd2291704e3330bf86870f376f043dca6dbe2
elinapark0818@gmail.com <elinapark0818@gmail.com>
2020-12-01 15:31:35 +0900

    여행 패키지 연습! 파일 위치 확인하기 (inspect)

--- com/yunjung/study/Company/travel/thailand.py
+++ com/yunjung/study/Company/travel/thailand.py
@@ -1,3 +1,11 @@
 class ThailandPackage:
     def detail(self):
         print("[태국 패키지 5박 7일] 방콕, 파타야 여행 130만원")
+
+if __name__ == "__main__":
+    print("Thailand 모듈을 직접 실행")
+    print("이 문장은 모듈을 직접 실행할 때만 실행돼요")
+    trip_to = ThailandPackage()
+    trip_to.detail()
+else:
+    print("Thailand 외부에서 모듈 호출")
--- com/yunjung/study/Company/travel_practice.py
+++ com/yunjung/study/Company/travel_practice.py
@@ -15,3 +15,8 @@ from travel import *
 # trip_to = vietnam.VietnamPackage()
 trip_to = thailand.ThailandPackage()
 trip_to.detail()
+
+import inspect
+import random
+print(inspect.getfile(random))
+print(inspect.getfile(thailand))
