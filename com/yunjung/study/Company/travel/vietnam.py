e41b4cee096579e18ee3fdcbe7b88f381420cda4
elinapark0818@gmail.com <elinapark0818@gmail.com>
2020-12-01 15:26:26 +0900

    여행 패키지 연습!

--- com/yunjung/study/Company/travel/vietnam.py
+++ com/yunjung/study/Company/travel/vietnam.py
@@ -1,3 +1,3 @@
-class vietnamPackage:
+class VietnamPackage:
     def detail(self):
         print("[베트남 패키지 3박 4일] 다낭 효도 여행 80만원")
--- com/yunjung/study/Company/travel_practice.py
+++ com/yunjung/study/Company/travel_practice.py
@@ -6,6 +6,12 @@
 # trip_to = ThailandPackage()
 # trip_to.detail()
 
-from travel.vietnam import vietnamPackage
-trip_to = vietnamPackage()
+# from travel.vietnam import VietnamPackage
+# trip_to = VietnamPackage()
+# trip_to.detail()
+
+# from random import *
+from travel import *
+# trip_to = vietnam.VietnamPackage()
+trip_to = thailand.ThailandPackage()
 trip_to.detail()
