e41c39f3f8120098e280c85670abb479e5fa4a49
elinapark0818@gmail.com <elinapark0818@gmail.com>
2020-12-01 15:17:53 +0900

    여행 패키지 연습!

--- com/yunjung/study/Company/travel_practice.py
+++ com/yunjung/study/Company/travel_practice.py
@@ -2,6 +2,10 @@
 # trip_to = travel.thailand.ThailandPackage()
 # trip_to.detail()
 
-from travel.thailand import ThailandPackage
-trip_to = ThailandPackage()
-trip_to.detail
+# from travel.thailand import ThailandPackage
+# trip_to = ThailandPackage()
+# trip_to.detail()
+
+from travel.vietnam import vietnamPackage
+trip_to = vietnamPackage()
+trip_to.detail()
