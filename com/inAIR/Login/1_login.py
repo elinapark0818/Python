6b1818cafeb46e94a8fae672f8cfe228bd8e2031
elinapark0818@gmail.com <elinapark0818@gmail.com>
2020-12-02 15:25:28 +0900

    셀레니움연습

---
+++ com/inAIR/Login/1_login.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+browser = webdriver.Chrome('/Users/jobflexqa2.MIDASIT/PycharmProjects/chromedriver_win32/chromedriver')
+browser.implicitly_wait(3)
+browser.get('https://st-ogu59-admin-recruiter-co-kr.midasweb.net/cus/login')
+
+browser.find_element_by_name('id').send_keys('qaqc09')  # 아이디 입력
+browser.find_element_by_name('password').send_keys('test1357@#')  # 비밀번호 입력
+
+browser.find_element_by_xpath('/html/body/form/div[1]/div[3]/div[1]/button').click()   # 로그인 버튼 클릭
+
+browser.find_element_by_xpath('/html/body/form/article/div/div[2]/button').click()    # 팝업창 닫기 클릭
+
+browser.find_element_by_xpath('/html/body/div/div[2]/div/section/a[2]/div/div/div[2]').click()    # [2.0] 클릭
+
+browser.find_element_by_xpath('/html/body/div[2]/button').click()  # 좌측 UI 열기
+
+browser.find_element_by_xpath('/html/body/div[2]/nav/ul/li[1]/a').click()  # 채용공고관리 클릭
+
+browser.find_element_by_xpath('/html/body/div[2]/nav/ul/li[1]/ul/li[2]/a').click()  # 채용공고등록 클릭
+
+browser.find_element_by_xpath('/html/body/div[2]/div/div/form/table/tbody/tr[1]/td/input').click()  # 공고명 텍스트박스 클릭
+
+browser.find_element_by_name('recruitNoticeName').send_keys('selenium_auto_title1')  # 공고명 입력하기
+
+browser.find_element_by_xpath('/html/body/div[2]/div/div/form/table/tbody/tr[2]/td/label/input').click()  # 기본이미지 체크박스 체크
+
+browser.find_element_by_xpath('/html/body/div[2]/div/div/form/table/tbody/tr[3]/td/span[1]/label/input').click()   # 일반채용 라디오버튼 클릭
+
+browser.find_element_by_xpath('/html/body/div[2]/div/div/form/table/tbody/tr[4]/td/label/select').click()  # 코드선택 셀렉박스 클릭
+
+import time
+time.sleep(2)
+
+from selenium.webdriver.support.select import Select
+select = Select(browser.find_element_by_id('frm-recruitClassCodeSn'))
+select.select_by_index(1)
+
+browser.find_element_by_xpath('/html/body/div[2]/div/div/form/table/tbody/tr[5]/td/div[2]/div[1]/label/select').click()
+
+import time
+time.sleep(2)
+
+from selenium.webdriver.support.select import Select
+select = Select(browser.find_element_by_name('depth1_1'))
+select.select_by_index(1)
+
+browser.find_element_by_xpath('/html/body/div[2]/div/div/form/table/tbody/tr[5]/td/div[2]/div[1]/ul/li[1]/label/span').click()
+
+browser.find_element_by_xpath('/html/body/div[2]/div/div/form/table/tbody/tr[5]/td/div[3]/span/button').click()
+
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+
+elem = browser.find_element_by_id("ReceiveStartYmd")
+browser.find_element_by_name('id').send_keys('2020.12.01')
+
+elem2 = browser.find_element_by_id("ReceiveEndYmd")
+browser.find_element_by_name('id').send_keys('2020.12.31')
+
+browser.find_element_by_xpath('/html/body/div[2]/div/div/form/table/tbody/tr[8]/td[2]/label[3]/input').click()
