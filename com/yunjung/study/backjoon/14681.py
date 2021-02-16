4bdef91a6dbd94400530be7935b89544bebee350
ElinaPark <elinapark0818@gmail.com>
2021-02-16 20:57:22 +0900

    n사분면 구하기

--- com/yunjung/study/backjoon/14681.py
+++ com/yunjung/study/backjoon/14681.py
@@ -11,15 +11,17 @@
 
 # if
 
-x = int(input("x값을 입력하세요."))
-y = int(input("y값을 입력하세요."))
-if x == 0 or y == 0:
-    print("0이 아닌 다른 수를 입력하세요.")
-elif x > 0 and y > 0:
-    print("제 1사분면입니다.")
-elif x < 0 and y > 0:
-    print("제 2사분면입니다.")
-elif x > 0 and y < 0:
-    print("제 4사분면입니다.")
+
+x = int(input())
+y = int(input())
+
+if x > 0:
+    if y > 0:
+        print(1)
+    else:
+        print(4)
 else:
-    print("제3사분면입니다.")
+    if y > 0:
+        print(2)
+    else:
+        print(3)
